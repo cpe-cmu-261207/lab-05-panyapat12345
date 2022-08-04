88a86dd266f10709785dd5fd33aa60a95444732b
Panyapat Wongdee <panyapat12345@gmail.com>
2022-08-04 15:59:52 +0700

    ans1

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  if (inputAdd.value === "") {
+    alert("Todo cannot be empty");
+    return;
+  }
+
+  addTodo(inputAdd.value, false);
+  saveTodo();
+
+  inputAdd.value = "";
 };
 
 function addTodo(title, completed) {
@@ -29,20 +38,66 @@ function addTodo(title, completed) {
   deleteBtn.className = "btn btn-danger";
 
   //your code here
-  //append todo to HTML...
-  //define buttons event...
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "") {
+      span.style.textDecoration = "line-through";
+    } else {
+      span.style.textDecoration = "";
+    }
+    saveTodo();
+  };
+
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
+
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+  todoCtn.prepend(div);
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    let todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
+  }
+
+  const dataRe = [];
+  for (let i = 0; i < data.length; i++) {
+    dataRe.push(data[data.length - 1 - i]);
   }
   //your code here
+  localStorage.setItem("TodoList", JSON.stringify(dataRe));
 }
 
 function loadTodo() {
   //your code here
+  const todoJson = localStorage.getItem("TodoList");
+  const data = JSON.parse(todoJson);
+  for (const todoDiv of data) {
+    console.log(todoDiv);
+    addTodo(todoDiv.title, todoDiv.completed);
+  }
 }
 
 loadTodo();
